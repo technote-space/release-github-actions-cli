c528f80f04013b2ca37196238c4e535aad7ca62c
Technote <technote.space@gmail.com>
2019-09-18 23:21:00 +0900

    feat: update script

--- bin/setup.sh
+++ bin/setup.sh
@@ -17,12 +17,12 @@ rm -rdf "${WORK_DIR}"
 mkdir -p "${WORK_DIR}"
 cd "${WORK_DIR}"
 
-#git clone "${CLONE_URL}" -b v1 --depth=1 "${PROGRAM_REPO}"
-#cd "${PROGRAM_REPO}"
-
-git clone "${CLONE_URL}" --depth=1 "${PROGRAM_REPO}"
+git clone "${CLONE_URL}" -b test/v1 --depth=1 "${PROGRAM_REPO}"
 cd "${PROGRAM_REPO}"
-yarn install
-yarn build
+
+#git clone "${CLONE_URL}" --depth=1 "${PROGRAM_REPO}"
+#cd "${PROGRAM_REPO}"
+#yarn install
+#yarn build
 
 cp "${current}"/fixture/*.js "${WORK_DIR}"/"${PROGRAM_REPO}"/
