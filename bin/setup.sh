980949af96958ba637391c95391b6673b71b3943
Technote <technote.space@gmail.com>
2019-09-19 14:47:40 +0900

    chore: remove comment

--- bin/setup.sh
+++ bin/setup.sh
@@ -20,9 +20,4 @@ cd "${WORK_DIR}"
 git clone "${CLONE_URL}" -b test/v1 --depth=1 "${PROGRAM_REPO}"
 cd "${PROGRAM_REPO}"
 
-#git clone "${CLONE_URL}" --depth=1 "${PROGRAM_REPO}"
-#cd "${PROGRAM_REPO}"
-#yarn install
-#yarn build
-
 cp "${current}"/fixture/*.js "${WORK_DIR}"/"${PROGRAM_REPO}"/
